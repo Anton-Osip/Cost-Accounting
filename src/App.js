fac69430a807058a4a1fdbb672e87fcc3f58c2c4
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2022-07-14 14:53:52 +0300

    created CostItem component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import CostItem from './components/CostItem'
+
 function App() {
 	return (
 		<div>
 			<h1>Начнем изучения JS</h1>
+			<p>Hey there!</p>
+			<CostItem />
 		</div>
 	)
 }
---
+++ src/components/CostItem.js
@@ -0,0 +1,9 @@
+function CostItem() {
+	return (
+		<div>
+			<h2>Элемент расхода</h2>
+		</div>
+	)
+}
+
+export default CostItem
