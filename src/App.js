cb37f165b7d797b9e49139de2c6d3c4bf119da20
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2022-07-14 17:07:13 +0300

    30- Альтернативный Синтаксис Функций

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Costs from './components/Costs'
+import Costs from './components/Costs/Costs'
 
-function App() {
+const App = () => {
 	const costs = [
 		{ date: new Date(2021, 2, 12), description: 'Холодильник', amount: 999.99 },
 		{ date: new Date(2021, 11, 25), description: 'MacBook', amount: 1254.72 },
--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,11 +1,11 @@
-import Card from './Card'
+import Card from '../UI/Card'
 import CostDate from './CostDate'
 import './CostItem.css'
 
-function CostItem(props) {
+const CostItem = props => {
 	return (
 		<Card className='cost-item'>
-			<CostDate date={props.date}/>
+			<CostDate date={props.date} />
 			<div className='cost-item__description'>
 				<h2>{props.description}</h2>
 				<div className='cost-item__price'>${props.amount}</div>
