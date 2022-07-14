840d860f5d9d8b2d0fcc988368a5faef42684519
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2022-07-14 19:09:10 +0300

    32- -Прослушивание- Событий и Работа с Обработчиками Событий

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -3,6 +3,11 @@ import CostDate from './CostDate'
 import './CostItem.css'
 
 const CostItem = props => {
+
+	const changeDescription = () => {
+		console.log('ghjk')
+	}
+	
 	return (
 		<Card className='cost-item'>
 			<CostDate date={props.date} />
@@ -10,6 +15,7 @@ const CostItem = props => {
 				<h2>{props.description}</h2>
 				<div className='cost-item__price'>${props.amount}</div>
 			</div>
+			<button onClick={changeDescription}>Изменить описание</button>
 		</Card>
 	)
 }
