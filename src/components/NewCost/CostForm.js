d0ce8304f4be43a875f22f8f3e630bf2cc1c17b8
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2022-07-16 06:02:20 +0300

    39- Использование Одного Состояния

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -2,18 +2,22 @@ import { useState } from 'react'
 import './CostForm.css'
 
 const CostForm = () => {
-	const [name, setName] = useState('')
-	const [amount, setAmount] = useState('')
-	const [date, setDate] = useState('')
-
+	// const [name, setName] = useState('')
+	// const [amount, setAmount] = useState('')
+	// const [date, setDate] = useState('')
+	const [userInput, setUserInput] = useState({
+		name: '',
+		amount: '',
+		date: '',
+	})
 	const nameChangeHandler = event => {
-		setName(event.target.value)
+		setUserInput({ ...userInput, name: event.target.value })
 	}
 	const amountChangeHandler = event => {
-		setAmount(event.target.value)
+		setUserInput({ ...userInput, amount: event.target.value })
 	}
 	const dateChangeHandler = event => {
-		setDate(event.target.value)
+		setUserInput({ ...userInput, date: event.target.value })
 	}
 
 	return (
@@ -21,7 +25,7 @@ const CostForm = () => {
 			<div className='new-cost__controls'>
 				<div className='new-cost__control'>
 					<label>Название</label>
-					<input type='text' onChange={nameChangeHandler} value={name} />
+					<input type='text' onChange={nameChangeHandler} value={userInput.name} />
 				</div>
 				<div className='new-cost__control'>
 					<label>Сумма</label>
@@ -30,7 +34,7 @@ const CostForm = () => {
 						min='0.01'
 						step='0.01'
 						onChange={amountChangeHandler}
-						value={amount}
+						value={userInput.amount}
 					/>
 				</div>
 				<div className='new-cost__control'>
@@ -40,7 +44,7 @@ const CostForm = () => {
 						min='2019-01-01'
 						max='2022-12-31'
 						onChange={dateChangeHandler}
-						value={date}
+						value={userInput.date}
 					/>
 				</div>
 				<div className='new-cost__actions'>
