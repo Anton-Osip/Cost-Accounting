85a9026c3ad0e12eeb797a16f7b3be448080bc6b
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2022-07-18 11:37:12 +0300

    40- Обновление Состояния, Зависящего от Предыдущего Состояния

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -2,22 +2,31 @@ import { useState } from 'react'
 import './CostForm.css'
 
 const CostForm = () => {
-	// const [name, setName] = useState('')
-	// const [amount, setAmount] = useState('')
-	// const [date, setDate] = useState('')
-	const [userInput, setUserInput] = useState({
-		name: '',
-		amount: '',
-		date: '',
-	})
+	const [name, setName] = useState('')
+	const [amount, setAmount] = useState('')
+	const [date, setDate] = useState('')
+	// const [userInput, setUserInput] = useState({
+	// 	name: '',
+	// 	amount: '',
+	// 	date: '',
+	// })
 	const nameChangeHandler = event => {
-		setUserInput({ ...userInput, name: event.target.value })
+		setName(event.target.value)
+		// setUserInput(previousState => {
+		// 	return { ...previousState, name: event.target.value }
+		// })
 	}
 	const amountChangeHandler = event => {
-		setUserInput({ ...userInput, amount: event.target.value })
+		setAmount(event.target.value)
+		// setUserInput(previousState => {
+		// 	return { ...previousState, amount: event.target.value }
+		// })
 	}
 	const dateChangeHandler = event => {
-		setUserInput({ ...userInput, date: event.target.value })
+		setDate(event.target.value)
+		// setUserInput(previousState => {
+		// 	return { ...previousState, date: event.target.value }
+		// })
 	}
 
 	return (
@@ -25,7 +34,7 @@ const CostForm = () => {
 			<div className='new-cost__controls'>
 				<div className='new-cost__control'>
 					<label>Название</label>
-					<input type='text' onChange={nameChangeHandler} value={userInput.name} />
+					<input type='text' onChange={nameChangeHandler} value={name} />
 				</div>
 				<div className='new-cost__control'>
 					<label>Сумма</label>
@@ -34,7 +43,7 @@ const CostForm = () => {
 						min='0.01'
 						step='0.01'
 						onChange={amountChangeHandler}
-						value={userInput.amount}
+						value={amount}
 					/>
 				</div>
 				<div className='new-cost__control'>
@@ -44,7 +53,7 @@ const CostForm = () => {
 						min='2019-01-01'
 						max='2022-12-31'
 						onChange={dateChangeHandler}
-						value={userInput.date}
+						value={date}
 					/>
 				</div>
 				<div className='new-cost__actions'>
