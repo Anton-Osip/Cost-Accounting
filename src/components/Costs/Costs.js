2707fc8ff8b18ddb663cb5d20435355170c57fe9
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2022-07-18 21:01:45 +0300

    54- Отображение Контента по Условию

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -5,26 +5,31 @@ import './Costs.css'
 import React, { useState } from 'react'
 
 const Costs = props => {
+	let costsContent = <p>В этом году расходов нет</p>
+
 	const [selectedYear, setSelectedYear] = useState('2021')
 	const yearChangeHandler = year => {
 		setSelectedYear(year)
 	}
-
+	
 	const filteredCosts = props.costs.filter(cost => {
 		return cost.date.getFullYear().toString() === selectedYear
 	})
 
+	if (filteredCosts.length > 0) {
+		costsContent = filteredCosts.map(cost => (
+			<CostItem
+				date={cost.date}
+				description={cost.description}
+				amount={cost.amount}
+				key={cost.id}
+			/>
+		))
+	}
 	return (
 		<Card className='costs'>
 			<CostsFilter onChangeYear={yearChangeHandler} year={selectedYear} />
-			{filteredCosts.map(cost => (
-				<CostItem
-					date={cost.date}
-					description={cost.description}
-					amount={cost.amount}
-					key={cost.id}
-				/>
-			))}
+			{costsContent}
 		</Card>
 	)
 }
