bdbce615a7925fe0f6507d514903014be4e5ed36
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2022-07-18 20:44:09 +0300

    52- Задание 3

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -5,21 +5,25 @@ import './Costs.css'
 import React, { useState } from 'react'
 
 const Costs = props => {
-	const [selectedYear, setSelectedYear] = useState('2019')
+	const [selectedYear, setSelectedYear] = useState('2021')
 	const yearChangeHandler = year => {
 		setSelectedYear(year)
 	}
 	return (
 		<Card className='costs'>
 			<CostsFilter onChangeYear={yearChangeHandler} year={selectedYear} />
-			{props.costs.map(cost => (
-				<CostItem
-					date={cost.date}
-					description={cost.description}
-					amount={cost.amount}
-					key={cost.id}
-				/>
-			))}
+			{props.costs.map(cost => {
+				if (cost.date.getFullYear() === Number(selectedYear)) {
+					return (
+						<CostItem
+							date={cost.date}
+							description={cost.description}
+							amount={cost.amount}
+							key={cost.id}
+						/>
+					)
+				}
+			})}
 		</Card>
 	)
 }
