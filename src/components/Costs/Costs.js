e64d8790787489374217513e3be9abc9511ca890
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2022-07-18 12:39:13 +0300

    46- Задание 2. Решение

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -2,32 +2,32 @@ import Card from '../UI/Card'
 import CostItem from './CostItem'
 import CostsFilter from './CostsFilter'
 import './Costs.css'
+import React, { useState } from 'react'
 
 const Costs = props => {
-	const saveChangeYearHandler = year => {
-		console.log(year)
+	const [selectedYear, setSelectedYear] = useState('2019')
+	const yearChangeHandler = year => {
+		setSelectedYear(year)
 	}
 	return (
-		<div>
-			<CostsFilter onChangeYear={saveChangeYearHandler} />
-			<Card className='costs'>
-				<CostItem
-					date={props.costs[0].date}
-					description={props.costs[0].description}
-					amount={props.costs[0].amount}
-				/>
-				<CostItem
-					date={props.costs[1].date}
-					description={props.costs[1].description}
-					amount={props.costs[1].amount}
-				/>
-				<CostItem
-					date={props.costs[2].date}
-					description={props.costs[2].description}
-					amount={props.costs[2].amount}
-				/>
-			</Card>
-		</div>
+		<Card className='costs'>
+			<CostsFilter onChangeYear={yearChangeHandler} year={selectedYear} />
+			<CostItem
+				date={props.costs[0].date}
+				description={props.costs[0].description}
+				amount={props.costs[0].amount}
+			/>
+			<CostItem
+				date={props.costs[1].date}
+				description={props.costs[1].description}
+				amount={props.costs[1].amount}
+			/>
+			<CostItem
+				date={props.costs[2].date}
+				description={props.costs[2].description}
+				amount={props.costs[2].amount}
+			/>
+		</Card>
 	)
 }
 
