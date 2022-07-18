722a9b21b763635fc79b24e7bd0e804a846024b2
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2022-07-18 21:13:56 +0300

    55- Возвращение Значения по Условию

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,35 +1,23 @@
 import Card from '../UI/Card'
-import CostItem from './CostItem'
 import CostsFilter from './CostsFilter'
 import './Costs.css'
 import React, { useState } from 'react'
+import CostsList from './CostsList'
 
 const Costs = props => {
-	let costsContent = <p>В этом году расходов нет</p>
-
 	const [selectedYear, setSelectedYear] = useState('2021')
 	const yearChangeHandler = year => {
 		setSelectedYear(year)
 	}
-	
+
 	const filteredCosts = props.costs.filter(cost => {
 		return cost.date.getFullYear().toString() === selectedYear
 	})
 
-	if (filteredCosts.length > 0) {
-		costsContent = filteredCosts.map(cost => (
-			<CostItem
-				date={cost.date}
-				description={cost.description}
-				amount={cost.amount}
-				key={cost.id}
-			/>
-		))
-	}
 	return (
 		<Card className='costs'>
 			<CostsFilter onChangeYear={yearChangeHandler} year={selectedYear} />
-			{costsContent}
+			<CostsList costs={filteredCosts} />
 		</Card>
 	)
 }
---
+++ src/components/Costs/CostsList.js
@@ -0,0 +1,21 @@
+import CostItem from './CostItem'
+import './CostsList.css'
+
+const CostsList = props => {
+	if (props.costs.length === 0) {
+		return <h2 className='cost-list__fallback'>В этом году расходов нет</h2>
+	}
+	return (
+		<ul className='cost-list'>
+			{props.costs.map(cost => (
+				<CostItem
+					date={cost.date}
+					description={cost.description}
+					amount={cost.amount}
+					key={cost.id}
+				/>
+			))}
+		</ul>
+	)
+}
+export default CostsList
